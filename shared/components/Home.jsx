dabfb5c7d0dad166806796f39c3b18c1c8c7b4f1
Nicholas Major <nick@nmajor.com>
2016-07-14 13:59:43 +0100

    Added the copy to the landing page.

--- shared/components/Home.jsx
+++ shared/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router';
+import { Link } from 'react-router';
 
 class Home extends Component { // eslint-disable-line
   render() {
@@ -20,24 +20,25 @@ class Home extends Component { // eslint-disable-line
         <div className="container">
           <div className="row">
             <h1>myemailbook.com</h1>
-            <h2>You have a story online. Print it now. Keep it forever.</h2>
+            <h2>You have a story in your emails. Publish it. Keep it forever.</h2>
             <br /><br />
-            <a href="#" className="btn btn-success">LEARN MORE</a>
-            <a href="#" className="btn btn-transparent">GET STARTED</a>
+            <Link to="/register" className="btn btn-transparent">CREATE AN ACCOUNT TO GET STARTED</Link>
           </div>
         </div>
       </div>
 
       <div className="container">
         <div className="row mt-row">
-          <div className="col-md-4 col-md-offset-2">
-            <h3>HOW DOES IT WORK?</h3>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-            <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-            <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <div className="col-md-5 col-md-offset-1">
+            <h3>WHY MIGHT YOU WANT AN EMAIL BOOK?</h3>
+            <p>You know that shoebox on the top shelf of your closet where you keep all your favorite letters people have written you over the years?</p>
+            <p>Probably not!</p>
+            <p>People dont write letters anymore. We Facebook message, we text, we snapchat, and sometimes we even email.</p>
+            <p>Most of the time those emails are meaningless. But sometimes there is something special. Sometimes there are whole threads of emails that actually mean a lot to you.</p>
+            <p>There is something special and cool about physical objects. So much of our lives are digital these days. When you build and publish your email book, you are converting an part of your digital life into something physical. Something touchable. Something keepable.</p>
           </div>
 
-          <div className="col-md-4">
+          <div className="col-md-6">
             <img role="presentation" style={{ marginTop: '90px' }} className="img-responsive" src="/img/bookstack.jpg" />
           </div>
 
@@ -48,14 +49,15 @@ class Home extends Component { // eslint-disable-line
         <div className="container">
           <div className="row">
             <div className="col-md-6 mac">
-              <img src="/img/bookinhand.jpg" className="img-responsive" role="presentation" />
+              <img style={{ marginTop: '30px' }} src="/img/bookinhand.jpg" className="img-responsive" role="presentation" />
             </div>
             <div className="col-md-4">
-              <h3>IMPORTANT STUFF HERE</h3>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-              <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-              <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-              <p><a href="#" className="btn btn-success">PURCHASE NOW</a></p>
+              <h3>WHAT IS MYEMAILBOOK.COM?</h3>
+              <p>MYEMAILBOOK.COM is a website that lets you build and publish a book from your emails.</p>
+              <p>Search and filter your emails and export them into our custom email book builder.</p>
+              <p>Edit, tweak and customize your emailbook, and check PDF previews so you know exactly what you are getting.</p>
+              <p>Finally, we will print and publish your book on demand and ship as many as your want to your door.</p>
+              <p><Link to="/register" className="btn btn-success">CREATE AN ACCOUNT TO GET STARTED</Link></p>
             </div>
           </div>
         </div>
@@ -64,49 +66,49 @@ class Home extends Component { // eslint-disable-line
       <div id="blackwrap">
         <div className="container">
           <div className="row">
-            <h2>OUR FEATURES</h2>
+            <h2>FEATURES</h2>
             <div className="col-md-4">
               <div className="block">
-                <span className="fa fa-calendar"></span>
-                <h4>Feature one</h4>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <span className="fa fa-link"></span>
+                <h4>Connect</h4>
+                <p>Emails spread across multiple email accounts? No problem, connect and export from as many accounts as you need.</p>
               </div>
             </div>
             <div className="col-md-4">
               <div className="block">
-                <span className="fa fa-cloud"></span>
-                <h4>Feature two</h4>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <span className="fa fa-search"></span>
+                <h4>Find</h4>
+                <p>Our filtering tools allow you to find emails by date range, subject line, sender, and receiver. So you can find all the emails you need for your emailbook.</p>
               </div>
             </div>
             <div className="col-md-4">
               <div className="block">
-                <span className="fa fa-database"></span>
-                <h4>Feature three</h4>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <span className="fa fa-edit"></span>
+                <h4>Edit</h4>
+                <p>Notice a typo? Thing a section of one email is not important? No problem. You can edit the body of any email. After all, your email book may outlast you.</p>
               </div>
             </div>
           </div>
           <div className="row" style={{ marginTop: '15px' }}>
             <div className="col-md-4">
               <div className="block">
-                <span className="fa fa-cubes"></span>
-                <h4>Feature four</h4>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <span className="fa fa-image"></span>
+                <h4>Photos</h4>
+                <p>Emails are more than just text these days. All photo attachments are automatically appended to the bottom of each email. </p>
               </div>
             </div>
             <div className="col-md-4">
               <div className="block">
-                <span className="fa fa-folder"></span>
-                <h4>Feature five</h4>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <span className="fa fa-file"></span>
+                <h4>Pages</h4>
+                <p>Your email book will be more than just a bundle of emails. Add a title page, a forward, and a table of contents to give your email book that professionally published feel.</p>
               </div>
             </div>
             <div className="col-md-4">
               <div className="block">
-                <span className="fa fa-suitcase"></span>
-                <h4>Feature six</h4>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <span className="fa fa-book"></span>
+                <h4>Publish</h4>
+                <p>On demand printing and professional book binding. We will ship your beautiful email book right to your door.</p>
               </div>
             </div>
           </div>
@@ -116,16 +118,11 @@ class Home extends Component { // eslint-disable-line
       <div id="gwrap">
         <div className="container">
           <div className="row">
-            <div className="col-md-4 col-md-offset-2">
-              <img className="img-responsive" src="/img/bookpile.jpg" role="presentation" />
-            </div>
-            <div className="col-md-4">
-              <h3>IMPORTANT STUFF HERE</h3>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-              <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-              <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-              <p>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.</p>
-              <p><a href="#" className="btn btn-success">PURCHASE NOW</a></p>
+            <div className="col-md-4 col-md-offset-4 text-center">
+              <h2>PRICING</h2>
+              <span className="fa fa-book price-book"></span>
+              <div className="big-price">40$</div>
+              <p><Link to="/register" className="btn btn-success">CREATE AN ACCOUNT TO GET STARTED</Link></p>
             </div>
           </div>
         </div>
@@ -133,15 +130,7 @@ class Home extends Component { // eslint-disable-line
 
       <div className="container">
         <div className="row mt-row">
-          <h3 style={{ textAlign: 'center' }}>TRY US NOW</h3>
-          <br />
-          <div className="col-md-6 col-md-offset-3">
-            <form role="form" action="register.php" method="post">
-              <input type="email" name="email" className="subscribe-input" placeholder="Enter your e-mail address..." required />
-              <button className="btn btn-submit subscribe-submit" type="submit">Subscribe</button>
-            </form>
-
-          </div>
+          <div style={{ textAlign: 'center' }}>&#169; myemailbook.com</div>
         </div>
       </div>
 
