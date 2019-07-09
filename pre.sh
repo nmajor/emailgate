b1463af7a6ab1481462aa9c9d09e8bb13a129caf
Nicholas Major <nick@nmajor.com>
2019-07-09 18:34:50 +0100

    Fixing pre.sh

--- pre.sh
+++ pre.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 # Write ssl cert and key to file for nginx
+
+mkdir -p /var/ssl/
+touch /var/ssl/server.crt
+touch /var/ssl/server.key
+
 echo $SSL_CRT | sed 's/\\n/\n/g' > /var/ssl/server.crt
 echo $SSL_KEY | sed 's/\\n/\n/g' > /var/ssl/server.key
